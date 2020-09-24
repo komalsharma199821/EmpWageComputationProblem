d1894e3d0e2c2098bd60847e481c2a6a444cf8cf
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-24 22:39:45 +0530

    Conditional Wage Calculation

--- WelcomeMessage.sh
+++ WelcomeMessage.sh
@@ -10,33 +10,36 @@ else
 	echo "Employee is Absent";
 fi
 
-EMPLOYEE_PER_HOUR_WAGE=20;
+EMPLOYEE_PER_HOUR_WAGE=100;
 FULL_DAY_HOUR=8;
 
+MAX_WORKING_DAYS=20;
+MAX_WORKING_HOURS=100;
+
+totalWorkingHrs=0;
+workingDays=1;
 
 function getWorkHours() {
         case $1 in
                 1) workHours=4;;
                 2) workHours=8;;
-                *) workHours=0;;
+		*) workHours=2;;
         esac
         echo $workHours;
 }
 
+while [[ $totalWorkingHrs -lt $MAX_WORKING_HOURS && $workingDays -lt $MAX_WORKING_DAYS ]]
+do
+	
+	resWorkDonePerDay=$( getWorkHours $((RANDOM%3)) );
+	#keep calculating the wage
+	wage=$((resWorkDonePerDay * EMPLOYEE_PER_HOUR_WAGE));
+	finalWage=$((finalWage + wage));
+	totalWorkingHrs=$((totalWorkingHrs + resWorkDonePerDay));
+	workingDays=$((workingDays + 1));
+done
 
-#calculation for daily wage
-if [ $value == 1 ]
-then
-        resWorkDonePerDay=$( getWorkHours $((RANDOM%3)) );
-
-        dailyWage=$((EMPLOYEE_PER_HOUR_WAGE * $resWorkDonePerDay * 20));
-        echo "Monthly Wage For an Employee is :"$dailyWage;
-
-else
-
-        echo "Empwage cannot be calculated for absent employee";
-fi;
-
+echo "Total Salary for the employee is :"$finalWage;
 
 
 
