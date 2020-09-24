820480fb1a1f595b8dbe4eaad2abb1d7816441d1
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-24 11:05:15 +0530

    Employee presentees program

--- WelcomeMessage.sh
+++ WelcomeMessage.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Problem Computation Program";
+
+value=$((RANDOM%2));
+echo $value;
+if [ $value == 1 ]
+then
+	echo "Employee is Present";
+else
+	echo "Employee is Absent";
+fi
+
