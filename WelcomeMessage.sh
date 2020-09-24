01a4d6c7f4f8e2c641758652393376c5f1014790
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-24 11:23:10 +0530

    Emp daily wage calculation

--- WelcomeMessage.sh
+++ WelcomeMessage.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 echo "Welcome to Employee Wage Problem Computation Program";
 
 value=$((RANDOM%2));
@@ -9,3 +10,14 @@ else
 	echo "Employee is Absent";
 fi
 
+EMPLOYEE_PER_HOUR_WAGE=20;
+FULL_DAY_HOUR=8;
+
+#calculation for daily wage
+if [ $value == 1 ]
+then
+	dailyWage=$((EMPLOYEE_PER_HOUR_WAGE * FULL_DAY_HOUR));
+	echo "Daily Wage For an Employee is :"$dailyWage;
+else
+	echo "Empwage cannot be calculated for absent employee";
+fi
